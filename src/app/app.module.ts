e7fb72de39b80dd3e0fa882a568f3aee35e60cf0
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-24 15:20:04 -0500

    MEAN2 Integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { RouterModule } from '@angular/router';
 
+import { DataService } from './services/dataservice/dataservice.service';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+export const firebase = {
+  apiKey: '<your-key>',
+  authDomain: '<your-project-authdomain>',
+  databaseURL: '<your-database-URL>',
+  storageBucket: '<your-storage-bucket>',
+  messagingSenderId: '<your-messaging-sender-id>'
+};
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    AngularFireModule.initializeApp(firebase),
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
